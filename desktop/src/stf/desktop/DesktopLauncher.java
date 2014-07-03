14ec8a7970296b0a47f4f5b43f3584f5e1b9487f
Clayton Hawken <claytonhawken@gmail.com>
2014-07-03 18:01:07 -0500

    Initial Project Setup

---
+++ desktop/src/stf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package stf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import stf.CatanGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CatanGDX(), config);
+	}
+}
---
+++ html/src/stf/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package stf.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import stf.CatanGDX;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CatanGDX();
+        }
+}
