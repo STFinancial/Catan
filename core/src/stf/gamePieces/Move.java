67665383dee1597dc00318d5b2a572a8d55e1ca7
STFinancial <palla021@umn.edu>
2014-07-06 17:41:25 -0500

    Created interface for move object

--- core/src/stf/gamePieces/Move.java
+++ core/src/stf/gamePieces/Move.java
@@ -1,13 +1,13 @@
 package stf.gamePieces;
 
 public class Move {
-	MoveType type;
-	MoveSubType subType;
+	private MoveType type;
+	private MoveSubType subType;
 	
-	int buildPosition;
-	DevelopmentCard playCard;
-	PortType portType;
-	Person tradePlayer;
+	private int buildPosition;
+	private DevelopmentCard playCard;
+	private PortType portType;
+	private Person tradePlayer;
 	
 	public Move(MoveType type) {
 		this.type = type;
@@ -21,9 +21,78 @@ public class Move {
 		this.buildPosition = buildPosition;
 	}
 	
+	//play constructor
 	public Move(MoveType type, MoveSubType subType, DevelopmentCard playCard) {
 		this.type = type;
 		this.subType = subType;
 		this.playCard = playCard;
+	}
+	
+	//port trade constructor
+	public Move(MoveType type, MoveSubType subType, PortType portType) {
+		this.type = type;
+		this.subType = subType;
+		this.portType = portType;
+	}
+	
+	//player trade constructor
+	public Move(MoveType type, MoveSubType subType, Person tradePlayer) {
+		this.type = type;
+		this.subType = subType;
+		this.tradePlayer = tradePlayer;
+	}
+	
+	public int getBuildPosition() {
+		return buildPosition;
+	}
+	
+	public DevelopmentCard getCard() {
+		return playCard;
+	}
+	
+	public PortType getPortType() {
+		return portType;
+	}
+	
+	public MoveSubType getSubType() {
+		return subType;
+	}
+	
+	public Person getTradePlayer() {
+		return tradePlayer;
+	}
+	
+	public MoveType getType() {
+		return type;
+	}
+	
+	
+	public void printInfo() {
+		String extra;
+		
+		switch (type) {
+		case BUILD: 
+			extra = String.valueOf(buildPosition);
+		break;
+		
+		case PLAY: 
+			extra = playCard.type.toString();
+		break;
+		
+		case TRADE: 
+			if (subType == MoveSubType.PORT) {
+			extra = portType.toString();
+			} else {
+			extra = tradePlayer.getColor().toString();
+			}
+		break;
+		
+		default:
+			extra = "";
+		break;
+		
+		}
+		
+		System.out.println(type.toString() + " " + subType.toString() + extra);
 	}
 }
