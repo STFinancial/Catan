8477366f27fe84849eabb95da653916bc5f2a311
STFinancial <palla021@umn.edu>
2014-07-06 16:38:00 -0500

    Fixing imports

--- desktop/src/stf/desktop/DesktopLauncher.java
+++ desktop/src/stf/desktop/DesktopLauncher.java
@@ -2,7 +2,7 @@ package stf.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import stf.CatanGDX;
+import stf.main.CatanGDX;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
